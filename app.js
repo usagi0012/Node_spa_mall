254476456643b95e8534c23b5dcbb88e646c8b0b
jiwon <usagi0012@naver.com>
2023-11-05 17:46:30 +0900

    모듈 설치/기본서버 열기

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 열렸어요!");
+});
